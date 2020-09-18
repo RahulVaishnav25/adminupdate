ed8661c9e90b976a0fc0fa2b49c18a38f92ed3b4
rahul vaishnav <rahulvaishnav166@gmail.com>
2020-09-18 18:05:15 +0530

    tp

--- WindowsFormsApp2/WindowsFormsApp2/Form2.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form2.Designer.cs
@@ -33,6 +33,7 @@
             this.gENERATEKEYToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.vIEWRECORDSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,7 +43,6 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -57,7 +57,8 @@
             this.vIEWRECORDSToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1060, 33);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(795, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -65,7 +66,7 @@
             // 
             this.gENERATEKEYToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gENERATEKEYToolStripMenuItem.Name = "gENERATEKEYToolStripMenuItem";
-            this.gENERATEKEYToolStripMenuItem.Size = new System.Drawing.Size(179, 29);
+            this.gENERATEKEYToolStripMenuItem.Size = new System.Drawing.Size(146, 24);
             this.gENERATEKEYToolStripMenuItem.Text = "GENERATE KEY";
             this.gENERATEKEYToolStripMenuItem.Click += new System.EventHandler(this.gENERATEKEYToolStripMenuItem_Click);
             // 
@@ -73,7 +74,7 @@
             // 
             this.vIEWRECORDSToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.vIEWRECORDSToolStripMenuItem.Name = "vIEWRECORDSToolStripMenuItem";
-            this.vIEWRECORDSToolStripMenuItem.Size = new System.Drawing.Size(181, 29);
+            this.vIEWRECORDSToolStripMenuItem.Size = new System.Drawing.Size(148, 24);
             this.vIEWRECORDSToolStripMenuItem.Text = "VIEW RECORDS";
             this.vIEWRECORDSToolStripMenuItem.Click += new System.EventHandler(this.vIEWRECORDSToolStripMenuItem_Click);
             // 
@@ -86,30 +87,36 @@
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(16, 36);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(12, 29);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1035, 501);
+            this.panel1.Size = new System.Drawing.Size(776, 407);
             this.panel1.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(347, 75);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(277, 26);
+            this.dateTimePicker1.TabIndex = 8;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(463, 330);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(347, 268);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(279, 30);
+            this.textBox2.Size = new System.Drawing.Size(210, 26);
             this.textBox2.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(235, 386);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(176, 314);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(211, 31);
+            this.label3.Size = new System.Drawing.Size(164, 25);
             this.label3.TabIndex = 6;
             this.label3.Text = "PRODUCT KEY";
             // 
@@ -117,20 +124,18 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
-            this.label2.Location = new System.Drawing.Point(269, 92);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(202, 75);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(176, 31);
+            this.label2.Size = new System.Drawing.Size(136, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "VALID UPTO";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(531, 144);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(398, 117);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 37);
+            this.button1.Size = new System.Drawing.Size(107, 30);
             this.button1.TabIndex = 2;
             this.button1.Text = "GENERATE";
             this.button1.UseVisualStyleBackColor = true;
@@ -139,41 +144,37 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(463, 387);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(347, 314);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(279, 30);
+            this.textBox1.Size = new System.Drawing.Size(210, 26);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(287, 329);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(215, 267);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 31);
+            this.label1.Size = new System.Drawing.Size(123, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "SERIAL NO";
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.button3);
             this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Location = new System.Drawing.Point(16, 53);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(12, 43);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1035, 484);
+            this.panel2.Size = new System.Drawing.Size(776, 393);
             this.panel2.TabIndex = 1;
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(11, 446);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(8, 362);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(117, 28);
+            this.button3.Size = new System.Drawing.Size(88, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "ACTIVATE KEY";
             this.button3.UseVisualStyleBackColor = true;
@@ -184,34 +185,24 @@
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1031, 438);
+            this.dataGridView1.Size = new System.Drawing.Size(773, 356);
             this.dataGridView1.TabIndex = 1;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.dateTimePicker1.Location = new System.Drawing.Point(463, 92);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(279, 30);
-            this.dateTimePicker1.TabIndex = 8;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(1060, 551);
+            this.ClientSize = new System.Drawing.Size(795, 448);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "ADMINISTRATOR";
             this.menuStrip1.ResumeLayout(false);
